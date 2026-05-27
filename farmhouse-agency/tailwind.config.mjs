96de00b0d5ae7010a8fed69384bcfaabe78f79d4
Kuhu Mishra <kuhu.mishra98188@gmail.com>
2026-05-27 16:52:39 +0530

    changes fix

--- farmhouse-agency/tailwind.config.mjs
+++ farmhouse-agency/tailwind.config.mjs
@@ -6,11 +6,20 @@ export default {
       animation: {
         'marquee': 'marquee 25s linear infinite',
         'marquee-fast': 'marquee 15s linear infinite',
+        'slide-1': 'slideshow 24s infinite',
+        'slide-2': 'slideshow 24s infinite 6s',
+        'slide-3': 'slideshow 24s infinite 12s',
+        'slide-4': 'slideshow 24s infinite 18s',
       },
       keyframes: {
         marquee: {
           '0%': { transform: 'translateX(0%)' },
           '100%': { transform: 'translateX(-100%)' },
+        },
+        slideshow: {
+          '0%, 20%': { opacity: '1' },
+          '25%, 95%': { opacity: '0' },
+          '100%': { opacity: '1' },
         },
       },
     },
